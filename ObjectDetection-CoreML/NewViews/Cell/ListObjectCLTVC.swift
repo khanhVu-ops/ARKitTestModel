05d6d1a9e015b07b54fcb5da945f530021d30163
Vũ Văn Khánh <khanhvv.bhsoft@gmail.com>
2022-09-07 12:02:07 +0700

    aloo

---
+++ ObjectDetection-CoreML/NewViews/Cell/ListObjectCLTVC.swift
@@ -0,0 +1,18 @@
+//
+//  ListObjectCLTVC.swift
+//  ObjectDetection-CoreML
+//
+//  Created by BHSoft on 07/09/2022.
+//  Copyright © 2022 tucan9389. All rights reserved.
+//
+
+import UIKit
+
+class ListObjectCLTVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ObjectDetection-CoreML/NewViews/ObjectModel.swift
@@ -0,0 +1,9 @@
+//
+//  ObjectModel.swift
+//  ObjectDetection-CoreML
+//
+//  Created by BHSoft on 07/09/2022.
+//  Copyright © 2022 tucan9389. All rights reserved.
+//
+
+import Foundation
