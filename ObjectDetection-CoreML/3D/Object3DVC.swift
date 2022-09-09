b3dddab6e452c344bbc60d4955dac663bdbb1f58
Vũ Văn Khánh <khanhvv.bhsoft@gmail.com>
2022-09-09 17:54:48 +0700

    fix interaction

--- ObjectDetection-CoreML/3D/Object3DVC.swift
+++ ObjectDetection-CoreML/3D/Object3DVC.swift
@@ -9,17 +9,22 @@
 import UIKit
 import ARKit
 class Object3DVC: UIViewController {
-
+    
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var cltvListObject: UICollectionView!
-    var itemSelected = "aa"
+    @IBOutlet weak var btnMeasure: UIButton!
+    var itemSelected = ""
     var currentNode = SCNNode()
-    let listObject: [ObjectModel] = [ObjectModel(pathObject3D: "art.scnassets/Plane", title: "Plane", image2DName: "Plane"),
-                                     ObjectModel(pathObject3D: "art.scnassets/Car", title: "Car", image2DName: "Car"),
-                                     ObjectModel(pathObject3D: "art.scnassets/Cup", title: "Cup", image2DName: "Cup"),
-                                     ObjectModel(pathObject3D: "art.scnassets/Shoes", title: "Shoes", image2DName: "Shoes"),
-                                     ObjectModel(pathObject3D: "art.scnassets/Guitar", title: "Guitar", image2DName: "Guitar"),
-                                     ObjectModel(pathObject3D: "art.scnassets/Chair", title: "Chair", image2DName: "Chair")]
+    let listObject: [ObjectModel] = [ObjectModel(pathObject3D: "vase.scn", title: "vase", image2DName: "vase"),
+                                     ObjectModel(pathObject3D: "sticky note.scn", title: "sticky note", image2DName: "sticky note"),
+                                     ObjectModel(pathObject3D: "painting.scn", title: "painting", image2DName: "painting"),
+                                     ObjectModel(pathObject3D: "lamp.scn", title: "lamp", image2DName: "lamp"),
+                                     ObjectModel(pathObject3D: "cup.scn", title: "cup", image2DName: "cup"),
+                                     ObjectModel(pathObject3D: "chair.scn", title: "chair", image2DName: "chair"),
+                                     ObjectModel(pathObject3D: "candle.scn", title: "candle", image2DName: "candle"),
+                                     ObjectModel(pathObject3D: "Center Table.obj", title: "Center Table", image2DName: "Center Table"),
+                                     ObjectModel(pathObject3D: "eb_house_plant_01.obj", title: "eb_house_plant_01", image2DName: "eb_house_plant_01"),
+                                     ObjectModel(pathObject3D: "IronMan.obj", title: "IronMan", image2DName: "IronMan")]
     
     enum BodyType : Int {
         case ObjectModel = 2;
@@ -28,23 +33,24 @@ class Object3DVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
-       
+        
     }
     
     func setUpView() {
-//        sceneView.delegate = self
+        //        sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
+        sceneView.debugOptions = [.showFeaturePoints]
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap(_ :)) )
         sceneView.addGestureRecognizer(tapGesture)
         
         let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(rotation))
         sceneView.addGestureRecognizer(rotationGesture)
         
-//        let moveGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(moved))
-//        self.sceneView.addGestureRecognizer(moveGestureRecognizer)
+        let scaleGesture = UIPinchGestureRecognizer(target: self, action: #selector(scaleObject(_:)))
+        
+        self.sceneView.addGestureRecognizer(scaleGesture)
         
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(moved(recognizer:)))
         self.sceneView.addGestureRecognizer(panGesture)
@@ -64,76 +70,37 @@ class Object3DVC: UIViewController {
         // Run the view's session
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
         sceneView.session.pause()
     }
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//
-//        //1. Get The Current Touch Point
-//        guard let currentTouchPoint = touches.first?.location(in: self.sceneView),
-//            //2. Get The Next Feature Point Etc
-//            let hitTest = sceneView.hitTest(currentTouchPoint, types: .existingPlane).first else { return }
-//
-//        //3. Convert To World Coordinates
-//        let worldTransform = hitTest.worldTransform
-//
-//        //4. Set The New Position
-//        let newPosition = SCNVector3(worldTransform.columns.3.x, worldTransform.columns.3.y, worldTransform.columns.3.z)
-//
-//        //5. Apply To The Node
-//        currentNode.simdPosition = float3(newPosition.x, newPosition.y, newPosition.z)
-//
-//    }
+  
     
-    func addObject(position: SCNVector3) {
-
+    func addObject(position: SIMD3<Float>) {
+        
         if itemSelected != "" {
-//
-            guard let scence = SCNScene(named: "Car" + ".scn") else {
-                            return
+            
+            guard let scence = SCNScene(named: self.itemSelected) else {
+                return
             }
             let modelNode = scence.rootNode
-//            guard let modelNode =  scence.rootNode.childNode(
-//                withName: "theNameOfTheParentNodeOfTheObject", recursively: true) else {
-//                return
-//            }
-
             modelNode.name = "Car"
-//            modelNode.categoryBitMask = BodyType.ObjectModel.rawValue
-//            modelNode.enumerateChildNodes { (node, _) in
-//                   node.categoryBitMask = BodyType.ObjectModel.rawValue
-//            }
-
             print(position)
             modelNode.simdPosition = SIMD3(position.x, position.y, position.z)
-            modelNode.simdScale = SIMD3(0.01, 0.01, 0.01)
-            currentNode = modelNode
+            modelNode.simdPosition = position
+            modelNode.simdScale = SIMD3(0.1, 0.1, 0.1)
+            self.currentNode = modelNode
             modelNode.enumerateChildNodes { (node, _) in
                 node.categoryBitMask = BodyType.ObjectModel.rawValue
             }
             modelNode.categoryBitMask = BodyType.ObjectModel.rawValue
-            sceneView.scene.rootNode.addChildNode(modelNode)
-//
-//            let objectModelScene = SCNScene(named:
-//                    "Car.scn")!
-////            let objectNode =  objectModelScene.rootNode
-//            guard let objectNode =  objectModelScene.rootNode.childNode(
-//                    withName: "car", recursively: true) else {
-//                return
-//            }
-//            objectNode.categoryBitMask = BodyType.ObjectModel.rawValue
-//            objectNode.enumerateChildNodes { (node, _) in
-//                    node.categoryBitMask = BodyType.ObjectModel.rawValue
-//                }
-//            objectNode.simdPosition = SIMD3(position.x, position.y, position.z)
-//            objectNode.simdScale = SIMD3(0.01, 0.01, 0.01)
-//                       currentNode = objectNode
-//            sceneView.scene.rootNode.addChildNode(objectNode)
-            itemSelected = ""
-//            myObjectNodes.insert(modelNode)
+            self.sceneView.scene.rootNode.addChildNode(modelNode)
+            self.itemSelected = ""
+            
+            //            myObjectNodes.insert(modelNode)
         }else {
             currentNode.simdPosition = SIMD3(position.x, position.y, position.z)
         }
@@ -151,41 +118,65 @@ class Object3DVC: UIViewController {
         return nil
     }
     
+    func checkVituralObjectExitsLocation(location: CGPoint, recognizerView: ARSCNView) -> SCNNode?{
+        
+        
+        let hitTestResults = self.sceneView.hitTest(location, options: [.categoryBitMask: BodyType.ObjectModel.rawValue])
+        for result in hitTestResults {
+            if let node1 = getParentNodeOf(result.node) {
+                return node1
+            }
+        }
+        return nil
+    }
     
     //MARK: @objc func
+    
+    @objc func scaleObject(_ gesture: UIPinchGestureRecognizer) {
+
+//        let location = gesture.location(in: sceneView)
+       
+        if gesture.state == .changed {
+
+            let pinchScaleX: CGFloat = gesture.scale * CGFloat((currentNode.scale.x))
+            let pinchScaleY: CGFloat = gesture.scale * CGFloat((currentNode.scale.y))
+            let pinchScaleZ: CGFloat = gesture.scale * CGFloat((currentNode.scale.z))
+            currentNode.scale = SCNVector3Make(Float(pinchScaleX), Float(pinchScaleY), Float(pinchScaleZ))
+            gesture.scale = 1
+
+        }
+        if gesture.state == .ended { }
+
+    }
+    
     @objc func didTap(_ gesture: UITapGestureRecognizer) {
         guard let scene = gesture.view as? ARSCNView else {return}
         let position = gesture.location(in: scene)
-//        checkNodeExistAtLocation(position: position)
-        guard let query = sceneView.raycastQuery(from: position, allowing: .estimatedPlane, alignment: .horizontal) else {
-            return
-        }
-        guard let hitFeature = sceneView.session.raycast(query).first else {
-            return
-        }
         
-        let hitTransform = SCNMatrix4(hitFeature.worldTransform)
-        let hitPosition = SCNVector3(hitTransform.m41,
-                                           hitTransform.m42,
-                                           hitTransform.m43)
-          
-        addObject(position: hitPosition)
+        // check virtual content exits location
+        
+        if let nodeExit = checkVituralObjectExitsLocation(location: position, recognizerView: scene) {
+            currentNode = nodeExit
+        }
+        else {
+            let results = scene.hitTest(position, types: .estimatedHorizontalPlane)
+            guard let hitFeature = results.first else {
+                return
+            }
+            
+//            let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+//            let hitPosition = SCNVector3(hitTransform.m41,
+//                                         hitTransform.m42,
+//                                         hitTransform.m43)
+            let touchLocation = hitFeature.worldTransform.translation
+            addObject(position: touchLocation)
+        }
+      
     }
     
-   
+    
     @objc func rotation(_ gesture: UIRotationGestureRecognizer) {
-//        let location = gesture.location(in: self.sceneView)
-//        if let result = sceneView.hitTest(location, options: nil ).first  {
-//            let chairNode = result.node
-//
-//            if gesture.state == .changed {
-//                chairNode.eulerAngles.y -= Float(gesture.rotation)
-//            }
-//            gesture.rotation = 0
-//
-//        }
-        
-        
+    
         if gesture.state == .changed {
             currentNode.eulerAngles.y -= Float(gesture.rotation)
         }
@@ -193,88 +184,51 @@ class Object3DVC: UIViewController {
         
     }
     
-    @objc func panGesture(_ recognizer: UIPanGestureRecognizer) {
-        guard let recognizerView = recognizer.view as? ARSCNView else { return }
-        let position = recognizer.location(in: recognizerView)
-//        checkNodeExistAtLocation(position: position)
-        guard let query = sceneView.raycastQuery(from: position, allowing: .estimatedPlane, alignment: .any) else {
-            return
-        }
-        guard let result = sceneView.session.raycast(query).first else {
-            return
-        }
-        currentNode.simdWorldPosition = result.worldTransform.translation
-        
-        let previousOrientation = currentNode.simdWorldTransform.orientation
-        let currentOrientation = result.worldTransform.orientation
-        currentNode.simdWorldOrientation = simd_slerp(previousOrientation, currentOrientation, 0.1)
-    }
-    
+   
+    var moveNode : SCNNode?
     @objc func moved(recognizer :UIPanGestureRecognizer) {
         
         guard let recognizerView = recognizer.view as? ARSCNView else { return }
         let touch = recognizer.location(in: recognizerView)
-        
-        let hitTestResults = self.sceneView.hitTest(touch, options: [.categoryBitMask: BodyType.ObjectModel.rawValue])
-//        guard let modelNodeHit = hitTestResults.lazy.compactMap({ result in
-//            result.node
-//        }).first else {
-//            return
-//        }
-        var modelNodeHit = SCNNode()
-        print(hitTestResults.count)
-//        guard let modelNodeHit = getParentNodeOf(hitTestResult.first?.node) else { return }
-//        guard let modelNodeHit = getParentNodeOf(hitTestResults.first?.node) else {
-//            return
-//        }
-        for result in hitTestResults {
-            if let node1 = getParentNodeOf(result.node) {
-                    modelNodeHit = node1
-                print("Oke")
-                break
+        print("Touch")
+     
+        if recognizer.state == .began {
+            if let nodePan = checkVituralObjectExitsLocation(location: touch, recognizerView: recognizerView) {
+                moveNode = nodePan
+                print("END Began")
+            }
+        }else if recognizer.state == .changed {
+            print("Changed")
+            guard let query = recognizerView.raycastQuery(from: touch, allowing: .estimatedPlane, alignment: .horizontal)else {
+                return
+            }
+            guard let planeHit = recognizerView.session.raycast(query).first else {
+                return
             }
             
-
-        }
-        
-//        guard let modelNodeHit = getParentNodeOf(hitTestResult.first?.node) else { return }
-        
-//        guard let query = sceneView.raycastQuery(from: touch, allowing: .existingPlaneGeometry, alignment: .any) else {
-//            return
-//        }
-//
-//        guard let result = sceneView.session.raycast(query).first?.anchor else {
-//            return
-//        }
-        
-        
-        
-        
-        var planeHit : ARHitTestResult!
-        
-        if recognizer.state == .changed {
-            
-            let hitTestPlane = self.sceneView.hitTest(touch, types: .existingPlane)
-            guard hitTestPlane.first != nil else { return }
-            planeHit = hitTestPlane.first!
-            print(planeHit)
-            print(modelNodeHit.name)
-            modelNodeHit.position = SCNVector3(planeHit.worldTransform.columns.3.x,modelNodeHit.position.y,planeHit.worldTransform.columns.3.z)
-            print(sceneView.anchor(for: modelNodeHit)?.name ?? "hi")
-            print(sceneView.anchor(for: modelNodeHit)?.transform.columns.3 ?? "lo")
-        
+            if let movingNode = moveNode {
+                movingNode.simdWorldPosition = planeHit.worldTransform.translation
+            }
+     
         }else if recognizer.state == .ended || recognizer.state == .cancelled || recognizer.state == .failed{
+            print("END")
             
-            guard let oldAnchor = sceneView.anchor(for: modelNodeHit) else { return }
-            let newAnchor = ARAnchor(name: oldAnchor.name!, transform: modelNodeHit.simdTransform )
-            print(oldAnchor.transform.columns.3)        // old position
-            sceneView.session.remove(anchor: oldAnchor)
-            sceneView.session.add(anchor: newAnchor)
-            print(newAnchor.transform.columns.3)        // updated position
-
+            if let movedNode = moveNode {
+                print("OK")
+                currentNode = movedNode
+                moveNode = nil
+                print("10")
+            }
+            
         }
     }
-
+    var tapMeasure = false
+    
+    @IBAction func didTapMeasure(_ sender: Any) {
+        btnMeasure.tintColor = tapMeasure ? .blue : .gray
+        tapMeasure = !tapMeasure
+    }
+    
 }
 extension Object3DVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -295,8 +249,8 @@ extension Object3DVC: UICollectionViewDelegate, UICollectionViewDataSource, UICo
         itemSelected = listObject[indexPath.row].pathObject3D
         
     }
-//    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-//        cltvListObject.deselectItem(at: indexPath, animated: true)
-//    }
+    //    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+    //        cltvListObject.deselectItem(at: indexPath, animated: true)
+    //    }
     
 }
